7d391dc68bd973c01a0f8079412abdf7dea7d2bd
Sam Honor <sjhonor@wpi.edu>
2025-04-26 17:41:02 -0400

    CBF works apparently

--- project-code/test_script.py
+++ project-code/test_script.py
@@ -7,24 +7,34 @@ from cbf_toolbox.geometry import Point
 from cbf_toolbox.safety import Simulation
 from cbf_toolbox.dynamics import SingleIntegrator
 from time import sleep
+import jax.numpy as jnp
+import sympy as sym
 
+# Build robot object
 dh_tab = [rtb.DHLink(d=96.326, alpha=-np.pi/2, a=0, offset=0),
               rtb.DHLink(d=0, alpha=0, a=np.sqrt(24**2 + 128**2), offset=-np.arctan2(128, 24)),
               rtb.DHLink(d=0, alpha=0, a=124, offset=np.arctan2(128, 24)),
               rtb.DHLink(d=0, alpha=0, a=133.4, offset=0)]
 robot = rtb.DHRobot(dh_tab, name='OMX-Arm')
-# robot.plot([0, 0, 0, 0], block=True)
-dyn = OMXArm(robot)
+symbot = rtb.DHRobot(dh_tab, name='OMX-arm-sym', symbolic=True)
+th1, th2, th3, th4 = sym.symbols('th1 th2 th3 th4')
+sym_jac = sym.simplify(symbot.jacob0([th1, th2, th3, th4]))
+lambda_jac = sym.lambdify([th1, th2, th3, th4], sym_jac, 'jax')
+dyn = OMXArm(robot, lambda_jac)
 
-agent = Agent(np.array([0, 0, 0, 0]), Point(4), dyn)
+# Create trajectory generator
+
+# Add everything to simulation
+agent = Agent(np.array([0.1, -np.pi/2, 0.2, -0.5]), Point(4), dyn)
 control = Goal(np.array([0, 0, 0, 0]), shape=Point(4), dynamics=SingleIntegrator(4))
 s = Simulation()
 s.add_agent(agent, control)
-# s.simulate(num_steps=100, dt = 0.1)
+s.add_obstacle(SingularityCBF(agent))
+
+# Simulate
 robot.plot(agent.state)
 for i in range(100):
     s.step(0.1)
-    # print(agent.state)
+    J = robot.jacob0(agent.state)
+    print(f' state: {agent.state}   manip: {jnp.linalg.det(lambda_jac(*agent.state)[0:3, 0:3]) - 10}')
     robot.plot(agent.state)
-    # sleep(0.1)
-    
